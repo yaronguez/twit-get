91fa9e82eca0390dd22bd6b00645d06d5de97139
Yaron Guez <yaron.guez@gmail.com>
2020-02-27 10:32:45 -0800

    refactored with recursive structure

--- twit-get-prompt.js
+++ twit-get-prompt.js
@@ -14,29 +14,24 @@ T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
     var tweets = data.statuses;
-    for (let i = 0; i < tweets.length; i++){
-        
-        if (tweets[i].text.includes(searchString)) {
-            //console.log(tweets[i].text);
-            let prompt = new confirm(`Do you want to delete "${tweets[i].text}"?`);
-            prompt.run()
-            .then(function (answer) {
-                console.log(answer);
-                if (answer == true) {
-                    console.log(`Deleting "${tweets[i].text}"`);
-                    
-                } else {
-                    console.log("Nope");
-                }
-            });
-            
-            //T.post('statuses/destroy/:id', { id: tweets[i].id_str }, function (err, data, response) {
-            //    console.log(data)
-            //})
-        }
-            
+    processTweets(tweets);
+}
 
-           
-    }
-
+function processTweets(tweets) {
+    if(tweets.length === 0)
+           return;
+    
+    const tweet = tweets[0];
+    const prompt = new confirm(`Do you want to delete "${tweet.text}"?`)
+        .run()
+        .then(function (answer) {
+            console.log(answer);
+            if (answer == true) {
+                console.log(`Deleting "${tweets[i].text}"`);
+                
+            } else {
+                console.log("Nope");
+            }
+        })
+        .finally(()=>{processTweets(tweets.slice(1))});
 }
